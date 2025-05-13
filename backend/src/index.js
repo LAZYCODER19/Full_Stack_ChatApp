82250ca4a36853e7fee34ada4aa31e1a27fdb197
LAZYCODER <dipghosal2002@gmail.com>
2025-05-13 19:58:44 +0530

    changed routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,28 +1,32 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import path from "path";
 
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-import { connectToBD } from './lib/db.js'
-import { server , app } from './lib/socket.js'
+import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(cookieParser())
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -32,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-server.listen(PORT,()=>{
-    console.log(`Sever is listening on port ${PORT}`)
-    connectToBD();
-})
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
